086c70bfce6eafa53fae0feb0108f745cb8617d6
Anthony Panozzo <anthony@rewardsnap.com>
2011-07-17 15:58:10 -0400

    Add basic chef directory structure.
    Also remove puppet stuff.

--- manifests/ubuntu1104.pp
+++
@@ -1,8 +0,0 @@
-class ubuntu1104 {
-  File { owner => 0, group => 0, mode => 0644 }
-  file { '/etc/motd':
-    content => "Welcome to your Vagrant-built virtual machine! Managed by Puppet.\n"
-  }
-}
-
-include ubuntu1104
